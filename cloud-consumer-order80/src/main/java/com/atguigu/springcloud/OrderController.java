3a7baff57311f6202bc7817770d9be0e484aa660
tiangy <617755200@qq.com>
2020-05-26 12:15:22 +0800

    提交自定义负载均衡

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderController.java
@@ -2,10 +2,18 @@ package com.atguigu.springcloud;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.LoadBalancer;
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -25,5 +33,39 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> get(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
+    {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
+    @Autowired
+    private LoadBalancer loadBalancer;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB()
+    {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if(instances == null || instances.size() <= 0)
+        {
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+
+        return new RestTemplate().getForObject(uri+"/payment/lb",String.class);
+
     }
 }
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
+    //使用自己定义的负载均衡类时，要把这个注销掉，因为这个会把http后面的域名看成是服务名称去处理，当然也可以自己定义一个Resttemplate对象
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
